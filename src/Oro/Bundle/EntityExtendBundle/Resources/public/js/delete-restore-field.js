742c43e1fa3764e69399829afd4f98b9f49a1c2e
Starikov Kirill <kirill.starikov@umbrella-web.com>
2014-08-07 20:27:20 +0400

    added alert

--- src/Oro/Bundle/EntityExtendBundle/Resources/public/js/delete-restore-field.js
+++ src/Oro/Bundle/EntityExtendBundle/Resources/public/js/delete-restore-field.js
@@ -1,14 +1,13 @@
 
-require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/modal', 'oroui/js/mediator'
-    ], function ($, _, __, Modal, mediator) {
+require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/modal', 'oroui/js/messenger', 'oroui/js/mediator'
+    ], function ($, _, __, Modal, messenger, mediator) {
     'use strict';
     $(function () {
-        $(document).on('click', '.custom-button-delete-field', function (e) {
+        $(document).on('click', '#oro_entity_config_type .custom-button-delete-field', function (e) {
             var el = $(this),
                 message = el.data('message'),
                 title = __('Field delete confirmation'),
-                content = '<p>' + __('Are you sure you want to delete this field?.') + '</p>' +
-                    '</p>' + __('Your config changes applied after update schema.') + '</p>',
+                content = '<p>' + __('Are you sure you want to delete this field?.') + '</p>',
                 /** @type oro.Modal */
                 confirmUpdate = new Modal({
                     allowCancel: true,
@@ -18,18 +17,19 @@ require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/modal
                     content: content
                 });
 
-            confirmUpdate.on('ok', deleteAction);
+            confirmUpdate.on('ok', function () {
+                execute('delete');
+            });
             confirmUpdate.open();
 
             return false;
         });
 
-        $(document).on('click', '.custom-button-restore-field', function (e) {
+        $(document).on('click', '#oro_entity_config_type .custom-button-restore-field', function (e) {
             var el = $(this),
                 message = el.data('message'),
                 title = __('Field restore confirmation'),
-                content = '<p>' + __('Are you sure you want to restore this field?.') + '</p>' +
-                    '</p>' + __('Your config changes applied after update schema.') + '</p>',
+                content = '<p>' + __('Are you sure you want to restore this field?.') + '</p>',
                 /** @type oro.Modal */
                 confirmUpdate = new Modal({
                     allowCancel: true,
@@ -39,30 +39,25 @@ require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/modal
                     content: content
                 });
 
-            confirmUpdate.on('ok', restoreAction);
-            confirmUpdate.open();
+            confirmUpdate.on('ok', function () {
+                execute('restore');
+            });
+            confirmUpdate.open();      
 
             return false;
-        });
-        function restoreAction() {
-            execute('restore');
-        }
-
-        function deleteAction() {
-            execute('delete');
-        }
+        });        
 
         function execute(action) {
 
-             var url, delimiter, modal, progress;
+            var url, delimiter, modal, progress;
 
             progress = $('#progressbar').clone();
             progress.removeAttr('id').find('h3').remove();
 
             modal = new Modal({
                 allowCancel: false,
-                title: __('title'),
-                content: __('content')
+                title: (action == 'delete') ? ('Deleting the field') : __('Restoring the field'),
+                content: __('It may take few minutes...')
             });
             modal.open();
             modal.$el.find('.modal-footer').html(progress);
@@ -73,30 +68,38 @@ require(['jquery', 'underscore', 'orotranslation/js/translator', 'oroui/js/modal
                     /* if the function will be called with a parameter to remove, replace the attributes of button to  the attributes of restore button */
                     var url = $('.custom-button-delete-field').attr('id');                                
                     $.post(url, function(deleteField) {
-                        if (deleteField) { 
+                        if ('successful' in deleteField) { 
                             url = url.replace("/remove/","/unremove/"); 
                             $('.custom-button-delete-field').addClass('btn-primary custom-button-restore-field');
                             $('.custom-button-restore-field').removeClass('btn-danger custom-button-delete-field');
                             $('.custom-button-restore-field').attr({'title':__('Restore field'), 'id': url});
                             $('.custom-button-restore-field').text(__('Restore'));
                             modal.close();
+                            mediator.execute('showMessage', 'success', __(deleteField.message), {flash: true});
                             return
-                        }              
+                        } else {
+                            modal.close();
+                            mediator.execute('showMessage', 'error', __('Unknown error'), {flash: true});
+                        }             
                     });
 
                     break
                 case 'restore':
                     /* if the function will be called with a parameter to restore, replace the attributes of button to  the attributes of delete button */
                     var url = $('.custom-button-restore-field').attr('id');                        
-                    $.post(url, function(deleteField) {
-                        if (deleteField) {  
+                    $.post(url, function(restoreField) {
+                        if ('successful' in restoreField) {  
                             url = url.replace("/unremove/","/remove/"); 
                             $('.custom-button-restore-field').addClass('btn-danger custom-button-delete-field');
                             $('.custom-button-delete-field').removeClass('btn-primary custom-button-restore-field');
                             $('.custom-button-delete-field').attr({'title':__('Delete field'), 'id': url});
                             $('.custom-button-delete-field').text(__('Delete'));
                             modal.close();
+                            mediator.execute('showMessage', 'success', __(restoreField.message), {flash: true});
                             return
+                        } else {
+                            modal.close();
+                            mediator.execute('showMessage', 'error', __('Unknown error'), {flash: true});
                         }              
                     });
                     break
